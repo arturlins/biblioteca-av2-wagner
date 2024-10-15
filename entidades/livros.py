1e3b771c43de15028a02d704767813785dd56699
Artur Lins <artur.olins@gmail.com>
2024-10-14 23:32:24 -0300

    Criada a lista de livros

---
+++ entidades/livros.py
@@ -0,0 +1,10 @@
+def lista_livros():
+    lista = [
+        {'titulo': 'Vidas Secas', 'autor': 'Graciliano Ramos', 'id': 1},
+        {'titulo': 'A Hora da Estrela', 'autor': 'Clarice Lispector', 'id': 2},
+        {'titulo': 'Dom Casmurro', 'autor': 'Machado de Assis', 'id': 3},
+        {'titulo': 'Grande Sertão: Veredas', 'autor': 'João Guimarães Rosa', 'id': 4},
+        {'titulo': 'Feliz Ano Novo', 'autor': 'Rubem Fonseca', 'id': 5}
+       
+    ]
+    return lista
