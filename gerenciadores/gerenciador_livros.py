fb0e62bb461bf5fd91a1fd99defe7500fc5b26c6
Artur Lins <artur.olins@gmail.com>
2024-10-15 17:16:16 -0300

    Adicionado módulo de listar livros

--- gerenciadores/gerenciador_livros.py
+++ gerenciadores/gerenciador_livros.py
@@ -2,11 +2,15 @@ from entidades.livros import lista_livros
 
 # listar livros cadastrados
 def listar_livros():
-    for livro in lista_livros:
-        print(f'{'id'} - {'titulo'}, de {'autor'}')
+    for livro in lista_livros():
+        print(f'ID {livro['id']}: {livro['titulo']}, de {livro['autor']}')
 
 # cadastrar livro
 
 # editar livro cadastrado
+def editar_livro():   
+    print("Digite o ID de qual cadastro de livro será editado: ")
+    listar_livros()
+
 
 # excluir livro cadastrado
--- main.py
+++ main.py
@@ -2,16 +2,22 @@ from entidades.livros import *
 from gerenciadores.gerenciador_livros import *
 
 def main():
+    print("Bem-vindo ao Sistema de Biblioteca do CESMAC\nEscolha uma opção abaixo: ")
+    print("Digite 1 para listar os livros")
     while True:
-        print("Bem-vindo ao Sistema de Biblioteca do CESMAC\nEscolha uma opção abaixo: ")
-        print("Digite 1 para listar os livros")
         try:
             opc = int(input("Selecione a opção: "))
-            if (opc == 1):
-                listar_livros()
-            if (opc == 2):
-                break
+            match opc:
+                case 1:
+                    listar_livros()
+                    break
+                case 2:
+                    editar_livro()
+                    break
+                case _:
+                    print("Opção inválida")    
+                    
         except ValueError:
-            print("Caracter inválido")
+            print("Opção inválida")
 
 main()
