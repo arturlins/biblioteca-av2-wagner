bf011f9721092c593eaeb270ca6227fdd73e7f50
Artur Lins <artur.olins@gmail.com>
2024-10-14 23:20:34 -0300

    Criada a lista de usuários

--- entidades/usuarios.py
+++ entidades/usuarios.py
@@ -2,6 +2,7 @@ def lista_usuarios():
     lista = [
         {'nome': 'João da Silva Santos', 'email': 'joaosilva@cesmac.edu.br', 'senha': '1234', 'id': 1},
         {'nome': 'Ana Bispo da Cunha', 'email': 'anacunha@cesmac.edu.br', 'senha': 'cesmac', 'id': 2},
-        {'nome': 'Maria Pereira Costa', 'email': 'mariacosta@cesmac.edu.br', 'senha': 'maria', 'id': 3}
+        {'nome': 'Maria Pereira Costa', 'email': 'mariacosta@cesmac.edu.br', 'senha': 'maria', 'id': 3},
+        {'nome': 'Ivo José Bulhões','email': 'ivobulhoes@cesmac.edu.br', 'senha': 'flamengo', 'id': 4}
     ]
     return lista
