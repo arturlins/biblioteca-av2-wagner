579055465264dce6de24a9d2d8de7803f733fe65
Artur Lins <artur.olins@gmail.com>
2024-10-15 15:39:58 -0300

    Estrutura do código refeita

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Bem-vindo ao Sistema de Biblioteca do CESMAC\nEscolha uma opção abaixo: ")
+from entidades.livros import *
+from gerenciadores.gerenciador_livros import *
+
+def main():
+    while True:
+        print("Bem-vindo ao Sistema de Biblioteca do CESMAC\nEscolha uma opção abaixo: ")
+        print("Digite 1 para listar os livros")
+        try:
+            opc = int(input("Selecione a opção: "))
+            if (opc == 1):
+                listar_livros()
+            if (opc == 2):
+                break
+        except ValueError:
+            print("Caracter inválido")
+
+main()
